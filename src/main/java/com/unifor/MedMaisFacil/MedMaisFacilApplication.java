7eec3d1cdf307f4c3587853d9afd603c719db99a
Davziinn <mdavi6085@gmail.com>
2026-08-20 10:38:29 -0300

    primeiro commit

---
+++ src/main/java/com/unifor/MedMaisFacil/MedMaisFacilApplication.java
@@ -0,0 +1,13 @@
+package com.unifor.MedMaisFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedMaisFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedMaisFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unifor/MedMaisFacil/MedMaisFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifor.MedMaisFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedMaisFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
